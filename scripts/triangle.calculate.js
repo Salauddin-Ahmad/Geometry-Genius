b3445f9e1284d24b4dffac5bb4eaed3bf6918319
Md. Salauddin <salauddinahmed555@gmail.com>
2024-02-13 03:11:28 +0600

    Added Triangle And Rectangle

---
+++ scripts/triangle.calculate.js
@@ -0,0 +1,22 @@
+function calculateTriangleArea() {
+    // get triangle base value
+    const triangleBaseInput = document.getElementById("triangle-base");
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText);
+    console.log( base);
+
+    // get triangle base value
+    const triangleHeightInput = document.getElementById("triangle-height");
+    const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightText);
+    console.log(height);
+    // calculate the area of the triangle
+    const area = 0.5 * base * height;
+    console.log('area of the triangle is', area);
+
+    // display the result
+    const triangleAreaSpan = document.getElementById("triangle-area");
+    triangleAreaSpan.innerText = area;
+
+
+}
