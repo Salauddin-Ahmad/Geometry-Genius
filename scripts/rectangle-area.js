81bb209ed639cba5c2a52645af6909bfc043674e
Md. Salauddin <salauddinahmed555@gmail.com>
2024-02-13 04:26:40 +0600

    added more shapes

--- scripts/rectangle-area.js
+++ scripts/rectangle-area.js
@@ -1,3 +1,19 @@
 function calculateRectangleArea() {
-    console.log('safsdf');
+    // get rectangle length value
+    const lengthInput = document.getElementById('rectangle-length');
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText);
+
+    // get rectangle width value
+    const widthInput = document.getElementById('rectangle-width');
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+    
+    // calculate the area of the rectangle
+    const area = length * width;
+    console.log('area of the rectangle is', area);
+
+    // display the result
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area; 
 }
--- scripts/triangle.calculate.js
+++ scripts/triangle.calculate.js
@@ -3,13 +3,13 @@ function calculateTriangleArea() {
     const triangleBaseInput = document.getElementById("triangle-base");
     const triangleBaseText = triangleBaseInput.value;
     const base = parseFloat(triangleBaseText);
-    console.log( base);
+console.log( base);
 
     // get triangle base value
     const triangleHeightInput = document.getElementById("triangle-height");
     const triangleHeightText = triangleHeightInput.value;
     const height = parseFloat(triangleHeightText);
-    console.log(height);
+console.log(height);
     // calculate the area of the triangle
     const area = 0.5 * base * height;
     console.log('area of the triangle is', area);
